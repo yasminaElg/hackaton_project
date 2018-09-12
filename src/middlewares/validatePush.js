027253734b4eee50209db5e97a88a1f6555fed78
Arnaud Gathy <Arnaud.gathy@gmail.com>
2018-09-12 23:05:40 +0200

    Added ok on github delivery

--- src/middlewares/validatePush.js
+++ src/middlewares/validatePush.js
@@ -4,6 +4,10 @@ import { runInNewContext } from "vm";
 export const validatePush = (req, res, next) => {
   const {body} = req
 
+  if(body.repository && !body.pusher) {
+    console.log("Not push but OK")
+    return res.sendStatus(200)
+  }
   if(github.user !== body.pusher.name || !github.repositories.find(({name}) => name === body.repository.name)) {
     console.log("Unauthorized")
     return res.sendStatus(401)
