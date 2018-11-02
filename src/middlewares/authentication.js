2c99101dc0848986c00a03d85773207b7bc8cdc0
Arnaud Gathy <33348914+ArnaudGathy@users.noreply.github.com>
2018-11-02 17:18:09 +0100

    Update authentication.js

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,7 +2,6 @@ import boom from 'boom'
 import {token} from "../constants/token";
 
 export const validateToken = (req, res, next) => {
-  console.log('req.headers', req.headers)
   if(req.headers.authorization !== token) {
     const boomed = boom.unauthorized('Invalid token ! You should add the authentication token to the authorization headers.')
     return res.status(boomed.output.statusCode).json(boomed.output.payload)
