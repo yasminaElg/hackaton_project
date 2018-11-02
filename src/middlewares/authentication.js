e29b0c98f6edaf7821f1510bb1eafb15b3b1feb0
Arnaud <arnaud.gathy@shippr.be>
2018-11-02 16:15:04 +0100

    added auth middleware

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,10 +1,10 @@
 import boom from 'boom'
 import {token} from "../constants/token";
 
-export const validateAuth = (req, res, next) => {
+export const validateToken = (req, res, next) => {
   console.log('req.headers', req.headers)
   if(req.headers.authorization !== token) {
-    const boomed = boom.unauthorized('Invalid token !')
+    const boomed = boom.unauthorized('Invalid token ! You should add the authentication token to the authorization headers.')
     return res.status(boomed.output.statusCode).json(boomed.output.payload)
   }
   next()
--- src/routes/system.js
+++ src/routes/system.js
@@ -1,11 +1,10 @@
 import expressPromiseRouter from 'express-promise-router'
-import {token} from "../constants/token";
-import {validateAuth} from "../middlewares/authentication";
+import {token} from "../constants/token"
 
 export const systemRouter = () => {
   const router = expressPromiseRouter()
 
-  router.get('/ping', validateAuth, (req, res) => res.send('pong'))
+  router.get('/ping', (req, res) => res.send('pong'))
   router.get('/login', (req, res) => res.json({token}))
 
   return router
